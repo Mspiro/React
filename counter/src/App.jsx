7ce51092ce22c62c84988df21b0c3b28d67b8227
Meeninath Dhobale <meeninath.dhobale@qed42.com>
2024-01-11 21:10:17 +0530

    Learning useState() method.

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Use state hook is used for update the counter variable in UI. It is handled by using the React only. In the below code the counter is the variable and set counter is the method which was getting in use to update that specific variable. the value 15 in the useState() is the default value for the counter variable.
+
+  const [counter, setCounter] = useState(15);
+  // This  addValue() function is used for the increasing the value of counter variable. Or we can say to update the state of the counter variable.
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+  // Similarly removeValue() function is used to reduce the value of the counter variable.
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
+    // This below line is used for the colletively pass return the all values. Because the return function only return the single value. So to manupulate that thing we are using the empty tag which having the maultiple children tags in it.
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h1>Hello Meeni</h1>
 
-export default App
+      <h2>Counter value: {counter} </h2>
+
+      <button onClick={addValue}>Add Value</button>
+
+      <br />
+
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+// This line is used to export the "App" component to use anywhere in the project. Here we are using it in the main.jsx file
+
+export default App; 
