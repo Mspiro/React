48524f0c12857012a9e62545ed80fc005d2c4a05
Meeninath Dhobale <meeninath.dhobale@qed42.com>
2024-01-11 20:18:25 +0530

    Learnign react basics

---
+++ learnreact/src/App.jsx
@@ -0,0 +1,13 @@
+import Test from "./test"
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello from Meeni</h1>
+      <Test />
+    </>
+  )
+}
+
+export default App
---
+++ learnreact/src/test.jsx
@@ -0,0 +1,9 @@
+
+
+function Test() {
+  return (
+    <h3>Hello I am testing.</h3>
+  )
+}
+
+export default Test
